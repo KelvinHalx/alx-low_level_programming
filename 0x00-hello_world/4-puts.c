204aee0e4f721fe0df607fb2be4b70bd6a85e1ae
KelvinHalx <kelvinwainaina03@gmail.com>
2022-03-10 20:35:04 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - Prints quote
- *
- * Return: zero on success
- *
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
