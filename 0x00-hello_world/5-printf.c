94641a7e2a95d4c3928d878518c4510e620236e7
KelvinHalx <kelvinwainaina03@gmail.com>
2022-03-10 20:36:57 +0300

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - Prints quote
- *
- * Return: zero on success
- *
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
