70c56b040f1c5e386bde5e20938451a8837c0a18
KelvinHalx <kelvinwainaina03@gmail.com>
2022-03-15 18:37:50 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - The last digit
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints last digit of random number and tells
+ * if it is greater than 5, 0, or less than 6 and not 0.
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
