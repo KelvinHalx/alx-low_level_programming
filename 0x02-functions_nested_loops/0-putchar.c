27716693aa95692c5adfcb4031d79f914c4a68fd
KelvinHalx <kelvinwainaina03@gmail.com>
2022-03-15 18:36:34 +0300

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - The last digit
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints last digit of random number and tells
+ * if it is greater than 5, 0, or less than 6 and not 0.
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{	
+
+	printf("Last digit of %d is %d ", n, (n % 10));
+
+	return (0);
+}
