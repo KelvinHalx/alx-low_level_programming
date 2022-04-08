50906dd1256e393c5b9baecc1336270d7c1c50ed
KelvinHalx <kelvinwainaina03@gmail.com>
2022-04-08 10:41:29 -0700

    Integers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,13 @@
 #include "main.h"
 #include <string.h>
-
 /**
  * print_rev - prints string backwards
  * @s: string
  * Return: void
  */
-
 void print_rev(char *s)
 {
   int i = 0;
-
 	while (s[i] != '\0')
 	{
 		i++;
@@ -21,6 +18,4 @@ void print_rev(char *s)
 		_putchar(s[i]);
 		i--;
 	}
->>>>>>> 2596676a92f6c8c5441104f05d3f0c0e0da3ef37
-	_putchar('\n');
 }
