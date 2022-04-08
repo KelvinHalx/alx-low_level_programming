2d38564f4708550a64762d5428633ab969dfddd3
KelvinHalx <kelvinwainaina03@gmail.com>
2022-04-08 07:09:23 -0700

    Allocation of memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * *malloc_checked - allocates memory using malloc
  * @b: unsigned int
  * Return: pointer
  */
-void *malloc_checked (unsigned int b)
+
+void *malloc_checked(unsigned int b)
 {
-  int *a;
-  a = malloc (b);
-  if (a == NULL)
-    {
-      exit (98);
-    }
-  return (a);
+	int *a;
+
+	a = malloc(b);
+	if (a == NULL)
+	{
+		exit(98);
+	}
+	return (a);
 }
