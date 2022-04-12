960f3a8057d8519d82cf238d5faa48b888eed259
KelvinHalx <kelvinwainaina03@gmail.com>
2022-04-12 18:22:54 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,12 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif /* _DOG_H_ */
